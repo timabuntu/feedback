27d5bafa406d1f8588a75c11f48dbdd832be08b5
thiago lopes de mello <timabuntu@gmail.com>
2022-05-04 10:26:30 -0300

    feat: added lib icons phosphor-react

---
+++ src/components/Widget.tsx
@@ -0,0 +1,9 @@
+import { ChatTeardropDots } from 'phosphor-react';
+
+export function Widget() {
+  return (
+    <button className='h-12 px-3 text-white rounded-full bg-violet-500'>
+      <ChatTeardropDots className='w-6 h-6'></ChatTeardropDots>
+    </button>
+  );
+}
