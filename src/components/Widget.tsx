cf7dd16c5ea82479daef6b99a747d19db9d07c0d
thiago lopes de mello <timabuntu@gmail.com>
2022-05-04 10:57:44 -0300

    feat: new color brand-500 in tailwind created

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,9 +1,20 @@
 import { ChatTeardropDots } from 'phosphor-react';
+import { useState } from 'react';
 
 export function Widget() {
+  const [] = useState();
+
   return (
-    <button className='h-12 px-3 text-white rounded-full bg-violet-500'>
-      <ChatTeardropDots className='w-6 h-6'></ChatTeardropDots>
-    </button>
+    <div className='absolute bottom-4 right-4'>
+      <p>Bem-Vindo Usuário</p>
+
+      <button className='flex items-center h-12 px-3 text-white rounded-full bg-brand-500 group'>
+        <ChatTeardropDots className='w-6 h-6' />
+
+        <span className='overflow-hidden transition-all duration-500 ease-linear max-w-0 group-hover:max-w-xs'>
+          <span className='pl-2'>Feedback</span>
+        </span>
+      </button>
+    </div>
   );
 }
