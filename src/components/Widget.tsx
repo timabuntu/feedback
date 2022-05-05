6b770ea24d5b7f2034a8ed9198d68ae4f7eb3e87
thiago lopes de mello <timabuntu@gmail.com>
2022-05-05 09:12:27 -0300

    feat: toggle icon feedback created

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -2,13 +2,19 @@ import { ChatTeardropDots } from 'phosphor-react';
 import { useState } from 'react';
 
 export function Widget() {
-  const [] = useState();
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
 
+  function toggleWidgetVisibility() {
+    setIsWidgetOpen(!isWidgetOpen);
+  }
   return (
     <div className='absolute bottom-4 right-4'>
-      <p>Bem-Vindo Usuário</p>
+      {isWidgetOpen && <p>Bem-Vindo Usuário</p>}
 
-      <button className='flex items-center h-12 px-3 text-white rounded-full bg-brand-500 group'>
+      <button
+        onClick={toggleWidgetVisibility}
+        className='flex items-center h-12 px-3 text-white rounded-full bg-brand-500 group'
+      >
         <ChatTeardropDots className='w-6 h-6' />
 
         <span className='overflow-hidden transition-all duration-500 ease-linear max-w-0 group-hover:max-w-xs'>
