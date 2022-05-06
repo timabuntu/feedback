b5cd1644f917eb04121024a7aa4ae5d37ea80c7f
thiago lopes de mello <timabuntu@gmail.com>
2022-05-06 10:57:50 -0300

    chore: added key in map

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -39,7 +39,10 @@ export function WidgetForm() {
       <div className='flex w-full gap-2 py-8'>
         {Object.entries(feedbackTypes).map(([key, value]) => {
           return (
-            <button className='flex flex-col items-center flex-1 w-24 gap-2 py-5 border-2 border-transparent rounded-lg focus:outline-none focus:border-brand-500 hover:border-brand-500 bg-zinc-800'>
+            <button
+              key={key}
+              className='flex flex-col items-center flex-1 w-24 gap-2 py-5 border-2 border-transparent rounded-lg focus:outline-none focus:border-brand-500 hover:border-brand-500 bg-zinc-800'
+            >
               <img src={value.image.source} alt={value.image.alt} />
               <span>{value.title}</span>
             </button>
