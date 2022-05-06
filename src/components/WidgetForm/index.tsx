e96d8f879419470638be230d2fa926a3629a0b54
thiago lopes de mello <timabuntu@gmail.com>
2022-05-06 10:29:40 -0300

    feat: images imported and added to an object

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,5 +1,33 @@
 import { CloseButton } from '../CloseButton';
 
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/idea.svg';
+import thoughtImageUrl from '../../assets/thought.svg';
+
+const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Imagem de um inseto, representando um bug',
+    },
+  },
+  IDEA: {
+    title: 'Idea',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Imagem de uma lâmpada, representando uma ideia',
+    },
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: 'Imagem de uma nuvem, representando um pensamento',
+      alt: thoughtImageUrl,
+    },
+  },
+};
+
 export function WidgetForm() {
   return (
     <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto'>
