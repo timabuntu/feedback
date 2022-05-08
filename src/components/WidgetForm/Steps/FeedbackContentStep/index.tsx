b0a00d32d83a82ff2d488f482ab6abc201c04720
thiago lopes de mello <timabuntu@gmail.com>
2022-05-08 18:31:45 -0300

    feat: content structure that receives the steps created

--- src/components/WidgetForm/Steps/FeedbackContentStep/index.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep/index.tsx
@@ -1,1 +1,13 @@
-export function FeedbackContentStep() {}
+import { CloseButton } from '../../../CloseButton';
+
+export function FeedbackContentStep({}) {
+  return (
+    <>
+      <header>
+        <span className='text-xl leading-6'>Deixe seu feedback</span>
+        <CloseButton />
+      </header>
+      <div className='flex w-full gap-2 py-8'></div>
+    </>
+  );
+}
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,11 +1,10 @@
 import { useState } from 'react';
 
-import { CloseButton } from '../CloseButton';
-
 import bugImageUrl from '../../assets/bug.svg';
 import ideaImageUrl from '../../assets/idea.svg';
 import thoughtImageUrl from '../../assets/thought.svg';
 import { FeedbackTypeStep } from './Steps/FeedbackTypeStep';
+import { FeedbackContentStep } from './Steps/FeedbackContentStep';
 
 export const feedbackTypes = {
   BUG: {
@@ -38,15 +37,10 @@ export function WidgetForm() {
 
   return (
     <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto'>
-      <header>
-        <span className='text-xl leading-6'>Deixe seu feedback</span>
-        <CloseButton />
-      </header>
-
       {!feedbackType ? (
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
       ) : (
-        <p>Hello World</p>
+        <FeedbackContentStep />
       )}
 
       <footer className='text-xs text-neutral-400'>
