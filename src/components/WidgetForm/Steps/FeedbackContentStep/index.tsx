e27b1b00f4b16aa6177a6c219dbdbe7c115e46f7
thiago lopes de mello <timabuntu@gmail.com>
2022-05-09 18:08:25 -0300

    feat: styled textarea form

--- src/components/WidgetForm/Steps/FeedbackContentStep/index.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep/index.tsx
@@ -37,7 +37,7 @@ export function FeedbackContentStep({
 
       <form className='w-full my-4'>
         <textarea
-          className='min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none border-zinc-600 bg-transparent rounded-md resize-none'
+          className='min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none border-zinc-600 bg-transparent rounded-md resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin'
           placeholder='Conte com detalhes o que está acontecendo...'
         ></textarea>
       </form>
