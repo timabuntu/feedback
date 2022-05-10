fc6f5ad2c43a09ce0caa0c6ea6541ce4c3b24188
thiago lopes de mello <timabuntu@gmail.com>
2022-05-10 11:32:59 -0300

    chore: scrollbar tailwind class conflict fixed

--- src/components/WidgetForm/Steps/FeedbackContentStep/index.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep/index.tsx
@@ -51,7 +51,7 @@ export function FeedbackContentStep({
 
       <form onSubmit={handleSubmitFeedback} className='w-full my-4'>
         <textarea
-          className='min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none border-zinc-600 bg-transparent rounded-md resize-none scrollbar  scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin'
+          className='min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none border-zinc-600 bg-transparent rounded-md resize-none  scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin'
           placeholder='Conte com detalhes o que está acontecendo...'
           onChange={(e) => setComment(e.target.value)}
           value={comment}
